9c00d7b42c3b451b7f1838202667e5b394a217b8
Igor Bezsmertnyi <mr.igor96@outlook.com>
2017-06-13 23:24:50 +0200

    add heroku Procfile

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get root" do
+    get root_path
     assert_response :success
   end
 
