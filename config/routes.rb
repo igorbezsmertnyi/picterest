544de50bae1373b1afb6d4ba045817c880bdc640
Igor Bezsmertnyi <mr.igor96@outlook.com>
2017-06-14 23:32:10 +0200

    add navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/faq' => 'home#faq'
+  get '/about' => 'home#about'
+  get '/contact' => 'home#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
