d742bef4f9c14bf053805e5b6c4cd44c98e13359
Igor Bezsmertnyi <mr.igor96@outlook.com>
2017-06-15 17:39:40 +0200

    add devise

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/logout', to: 'devise/sessions#destroy'
+    get '/sign-up', to: 'devise/registrations#new'
+  end
+
   root 'home#index'
   get '/faq' => 'home#faq'
   get '/about' => 'home#about'
