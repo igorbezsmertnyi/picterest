9ca610a6945cb7c70cadc340a9d9f8be1b4ffa70
mr_stig <mr.igor96@outlook.com>
2017-06-23 00:24:14 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
+end
+
+pics = [
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' },
+  { description: 'lorem ipsum' },
+  { description: 'dolerem ipsum' },
+  { description: 'consectetur adipiscing elit' },
+  { description: 'ed accumsan neque erat' },
+  { description: 'in pretium ipsum sollicitudin ac' }
+]
+
+pics.each.with_index do |attributes, i|
+  attributes[:image] = seed_image(i + 1)
+  User.first.pics.create(attributes)
+end
