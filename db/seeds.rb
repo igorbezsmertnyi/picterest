2e361bc0235160800860ef6419553258f6c1592b
mr_stig <mr.igor96@outlook.com>
2017-06-23 00:28:44 +0200

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 def seed_image(file_name)
-  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
+  if Rails.env.development?
+    assets_path = "/app/assets/images/seed/"
+  else
+    assets_path = "/assets/image/seed/"
+  end
+
+  File.open(File.join(Rails.root, "#{assets_path}#{file_name}.jpg"))
 end
 
 pics = [
@@ -55,14 +61,12 @@ pics = [
   { description: 'consectetur adipiscing elit' },
   { description: 'ed accumsan neque erat' },
   { description: 'in pretium ipsum sollicitudin ac' },
-  { description: 'lorem ipsum' },
-  { description: 'dolerem ipsum' },
-  { description: 'consectetur adipiscing elit' },
-  { description: 'ed accumsan neque erat' },
-  { description: 'in pretium ipsum sollicitudin ac' }
+  { description: 'lorem ipsum' }
 ]
 
-pics.each.with_index do |attributes, i|
-  attributes[:image] = seed_image(i + 1)
+# binding.pry_remote
+
+pics.each_with_index do |attributes, i|
+  attributes[:image] = seed_image(i + 1) if i + 1 <= pics.size - 1
   User.first.pics.create(attributes)
 end
